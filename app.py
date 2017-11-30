67481bfb26d37d0edf39fa29657fc7aa654bbebc
shivakishore14 <shivakishore14@gmail.com>
2017-11-30 16:10:45 +0530

    added database module

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, jsonify, render_template, request
-import numpy as np
-import tensorflow as tf
 
+import numpy as np
+import os
+import tensorflow as tf
+from db import DataStore
 
 from mnist import model
 
 
+db = DataStore()
+
 x = tf.placeholder("float", [None, 784])
 sess = tf.Session()
+
+
 
 # restore trained data
 with tf.variable_scope("perceptron"):
@@ -19,12 +25,13 @@ with tf.variable_scope("regression"):
 with tf.variable_scope("convolutional"):
     keep_prob = tf.placeholder(tf.float32)
     y_conv, conv_variables = model.convolutional(x, keep_prob)
+
 with tf.variable_scope("rnn"):
     y_rnn,_ = model.rnn_network(x)
 rnn_variables = tf.get_collection(tf.GraphKeys.VARIABLES, scope='rnn')
+
 saver = tf.train.Saver(conv_variables+perceptron_variables+regression_variables+rnn_variables)
 saver.restore(sess, "mnist/data/mnist.ckpt")
-
 
 def regression(input):
     return sess.run(tf.nn.softmax(y_reg), feed_dict={x: input}).flatten().tolist()
@@ -43,9 +50,9 @@ def rnn(input):
 # webapp
 app = Flask(__name__)
 
-
 @app.route('/api/mnist', methods=['POST'])
 def mnist():
+    db.update_key('prediction_reqs')
     input = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 784)
     output1 = regression(input)
     output2 = convolutional(input)
@@ -53,10 +60,19 @@ def mnist():
     output4 = rnn(input)
     return jsonify(results=[output1, output2, output3, output4])
 
+@app.route('/visit_stats')
+def visit_stats():
+    data = db.get_data_from_db()
+    return jsonify(results=data)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    db.update_key('visits')
+    resp = app.make_response(render_template('index.html'))
+    if 'is_old_user' not in request.cookies:
+        resp.set_cookie('is_old_user', 'True')
+        sb.update_key('uniq_visits')
+    return resp
 
 
 if __name__ == '__main__':
---
+++ db.py
@@ -0,0 +1,50 @@
+import psycopg2
+import os
+import urlparse
+
+class DataStore:
+    def __init__(self):
+        DATABASE_URL = 'postgres://mnistapp:mnistapppass@localhost/mnistappdb'
+        urlparse.uses_netloc.append("postgres")
+        url = urlparse.urlparse(DATABASE_URL)
+
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+        CREATE_TABLE = "CREATE TABLE IF NOT EXISTS key_val_store( key_ text , value_ int);"
+        CREATE_ROW = "INSERT INTO key_val_store(key_, value_) SELECT '%s' , 0 WHERE '%s' NOT IN ( SELECT key_ FROM key_val_store);"
+        cur = conn.cursor()
+        keys = ["visits", "uniq_visits", "prediction_reqs"]
+        for k in keys:
+            cur.execute(CREATE_ROW%(k,k))
+        self.cur = cur
+        self.conn = conn
+        self.commit()
+
+    def commit(self):
+        self.conn.commit()
+
+    def get_data_from_db(self):
+        key_val = {}
+        try:
+            self.cur.execute("""SELECT * from key_val_store""")
+        except:
+            print "Error inserting to table"
+
+        rows = self.cur.fetchall()
+        for row in rows:
+            key_val[row[0]] = row[1]
+        return key_val
+        self.commit()
+
+    def update_key(self, key):
+        try:
+            query = "UPDATE key_val_store SET value_ = value_ + 1 WHERE key_ = '%s'"%(key)
+            self.cur.execute(query)
+        except:
+            print "Update Error"
+        self.commit()
